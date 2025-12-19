78b449ad47e7b0f8cd24e12e23f495f8e69cd2e2
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-12-19 13:56:32 +0100

    add main form App integrating all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './styles/App.css';
+import Personal from './components/Personal';
+import Education from './components/Education';
+import Experience from './components/Experience';
 
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Personal />
+      <Education />
+      <Experience />
+    </>
+  );
 }
 
 export default App;

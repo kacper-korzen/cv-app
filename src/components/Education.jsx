90e79defad8bbde8e43d87ade61847a15dfa9dac
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-12-19 22:21:36 +0100

    move data to Home component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,37 +1,11 @@
 import Field from './Field';
 
-const FIELDS = [
-  {
-    fieldName: 'Uczelnia',
-    id: 'school-' + crypto.randomUUID(),
-    type: 'text',
-    placeholder: 'Nazwa uczelni',
-  },
-  {
-    fieldName: 'Stopień studiów',
-    id: 'studiesTitle-' + crypto.randomUUID(),
-    type: 'text',
-    placeholder: 'Wpisz stopień/temat studiów',
-  },
-  {
-    fieldName: 'Data rozpoczęcia',
-    id: 'startDate-' + crypto.randomUUID(),
-    type: 'date',
-    placeholder: '08/2024',
-  },
-  {
-    fieldName: 'Data zakończenia',
-    id: 'endDate-' + crypto.randomUUID(),
-    type: 'date',
-  },
-];
-
-function Education() {
+function Education(fields) {
   return (
     <fieldset className="education-section">
       <legend>Edukacja</legend>
-      {FIELDS.map((field) => (
-        <Field key={field.id} fieldInfo={{ ...field }} />
+      {fields.map((field) => (
+        <Field key={field.id} fieldInfo={field} />
       ))}
     </fieldset>
   );

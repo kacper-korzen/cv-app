7c2e7c82d40b864d0eb9dc86b288247e07366e30
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-12-19 13:36:51 +0100

    add Personal form component with basic fields

---
+++ src/components/Personal.jsx
@@ -0,0 +1,39 @@
+import Field from './Field';
+const FIELDS = [
+  {
+    fieldName: 'Imie i nazwisko',
+    id: 'fname',
+    type: 'text',
+    placeholder: 'John Doe',
+  },
+  {
+    fieldName: 'Email',
+    id: 'email',
+    type: 'email',
+    placeholder: 'johndoe@gmail.com',
+  },
+  {
+    fieldName: 'Telefon',
+    id: 'phone',
+    type: 'tel',
+    placeholder: '123 345 654',
+  },
+  {
+    fieldName: 'Adres',
+    id: 'address',
+    type: 'text',
+    placeholder: 'Miasto, Kraj',
+  },
+];
+
+function Personal() {
+  return (
+    <div className="personal-section">
+      {FIELDS.map((field) => (
+        <Field key={field.id} fieldInfo={{ ...field }} />
+      ))}
+    </div>
+  );
+}
+
+export default Personal;

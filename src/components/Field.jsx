dd5cd19d87019dd7425e3d71d2469716fc6e3d43
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-12-19 14:04:15 +0100

    delete br from component and add styles

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,9 +1,9 @@
+import '../styles/Field.css';
+
 function Field({ fieldInfo }) {
   return (
     <div className="field-container">
       <label htmlFor={fieldInfo.id}>{fieldInfo.fieldName}</label>
-      <br />
-
       <input
         type={fieldInfo.type}
         id={fieldInfo.id}

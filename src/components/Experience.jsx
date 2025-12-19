02ac96d80fc706c6d29b3d4186edcc07c221f943
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-12-19 22:21:58 +0100

    move data to home component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,42 +1,11 @@
 import Field from './Field';
 
-const FIELDS = [
-  {
-    fieldName: 'Nazwa firmy',
-    id: 'firma-' + crypto.randomUUID(),
-    type: 'text',
-    placeholder: 'Firma Inc',
-  },
-  {
-    fieldName: 'Stanowisko',
-    id: 'positionTitle-' + crypto.randomUUID(),
-    type: 'text',
-    placeholder: 'Opisz stanowisko w pracy',
-  },
-  {
-    fieldName: 'Opis',
-    id: 'description-' + crypto.randomUUID(),
-    type: 'text',
-    placeholder: 'Opis obowiązków ',
-  },
-  {
-    fieldName: 'Data rozpoczęcia',
-    id: 'startDate-' + crypto.randomUUID(),
-    type: 'date',
-  },
-  {
-    fieldName: 'Data zakończenia',
-    id: 'endDate-' + crypto.randomUUID(),
-    type: 'date',
-  },
-];
-
-function Experience() {
+function Experience({ fields }) {
   return (
     <fieldset className="experience-section">
       <legend>Praca</legend>
-      {FIELDS.map((field) => (
-        <Field key={field.id} fieldInfo={{ ...field }} />
+      {fields.map((field) => (
+        <Field key={field.id} fieldInfo={field} />
       ))}
     </fieldset>
   );

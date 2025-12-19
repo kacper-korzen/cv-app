c7ec1788619663c8c856bbd4002a03e2be13118d
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-12-19 22:19:53 +0100

    move data to home component

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,39 +1,15 @@
 import Field from './Field';
-const FIELDS = [
-  {
-    fieldName: 'Imie i nazwisko',
-    id: 'fname',
-    type: 'text',
-    placeholder: 'John Doe',
-  },
-  {
-    fieldName: 'Email',
-    id: 'email',
-    type: 'email',
-    placeholder: 'johndoe@gmail.com',
-  },
-  {
-    fieldName: 'Telefon',
-    id: 'phone',
-    type: 'tel',
-    placeholder: '123 345 654',
-  },
-  {
-    fieldName: 'Adres',
-    id: 'address',
-    type: 'text',
-    placeholder: 'Miasto, Kraj',
-  },
-];
 
-function Personal() {
+function Personal({ fields }) {
   return (
-    <fieldset className="personal-section">
-      <legend>Dane osobowe</legend>
-      {FIELDS.map((field) => (
-        <Field key={field.id} fieldInfo={{ ...field }} />
-      ))}
-    </fieldset>
+    <form action="">
+      <fieldset className="personal-section">
+        <legend>Dane osobowe</legend>
+        {fields.map((field) => (
+          <Field key={field.id} fieldInfo={field} />
+        ))}
+      </fieldset>
+    </form>
   );
 }
 

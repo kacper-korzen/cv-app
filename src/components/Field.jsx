0a0e443bf0411b06cec24e8797f9a24ba27292b6
Kacper <[k.kacper.korzeniewski@gmail.com]>
2025-12-31 03:07:10 +0100

    change component to simpler version

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,16 +1,16 @@
 import '../styles/Field.css';
 
-function Field({ fieldInfo, value, onChange }) {
+function Field({ fieldInfo, value, onChange, className }) {
   return (
     <div className="field-container">
-      <label htmlFor={fieldInfo.id}>{fieldInfo.fieldName}</label>
+      <span>{fieldInfo.icon}</span>
       <input
         type={fieldInfo.type}
         id={fieldInfo.id}
         placeholder={fieldInfo.placeholder}
         value={value}
         onChange={(e) => onChange(fieldInfo.id, e.target.value)}
-        className="field-input"
+        className={className}
       />
     </div>
   );

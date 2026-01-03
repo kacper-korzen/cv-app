298a4a78eef03cfb0ffd6835269ecbf9a51054e3
Kacper <[k.kacper.korzeniewski@gmail.com]>
2026-01-03 23:20:05 +0100

    add styles and wrap entry-container in frragment

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,20 +1,23 @@
 import Button from './Button';
 import Field from './Field';
+import '../styles/Entry.css';
 
 function Entry({ fields, entry, onChange, onRemove, fieldClassName }) {
   return (
-    <div key={entry.id} className="entry-container">
-      {fields.map((field) => (
-        <Field
-          key={field.id}
-          fieldInfo={field}
-          value={entry[field.id] || ''}
-          onChange={(id, val) => onChange(entry.id, id, val)}
-          className={fieldClassName}
-        />
-      ))}
+    <>
+      <div key={entry.id} className="entry-container">
+        {fields.map((field) => (
+          <Field
+            key={field.id}
+            fieldInfo={field}
+            value={entry[field.id] || ''}
+            onChange={(id, val) => onChange(entry.id, id, val)}
+            className={fieldClassName}
+          />
+        ))}
+      </div>
       <Button children={'Usuń'} variant={'delete'} onClick={onRemove} />
-    </div>
+    </>
   );
 }
 
